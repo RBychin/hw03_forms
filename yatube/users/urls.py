b46ff43443ec4b4c18573b6d9af56c2441354d18
rbychin <r.bychine@gmail.com>
2022-11-10 23:51:52 +0300

    this is some kind of hell

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,4 +1,12 @@
-from django.contrib.auth.views import *
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetCompleteView,
+    PasswordResetConfirmView,
+    PasswordChangeView
+)
 from django.urls import path
 from . import views
 
@@ -6,11 +14,11 @@ app_name = 'users'
 
 urlpatterns = [
     path(
-      'logout/',
-      LogoutView.as_view(
-          template_name='users/logged_out.html'
-      ),
-      name='logout'
+        'logout/',
+        LogoutView.as_view(
+            template_name='users/logged_out.html'
+        ),
+        name='logout'
     ),
     path(
         'signup/',
